1237b71fa777e9d9333f8701c0e577e48cdafd03
elias <99310710+bo-codes@users.noreply.github.com>
2023-02-09 22:19:57 -0800

    created header and navbar components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import NavBar from "../NavBar/NavBar";
+import "./Header.css"
+
+function Header() {
+  return (
+    <div id="screen">
+      <NavBar />
+      <div id="screen-text">B0_C0DES</div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import "./NavBar.css"
+
+function NavBar() {
+  return (
+    <div id="navbar-container">
+      <div id="list-div">
+        <ul>
+          <li>About</li>
+          <li>Experience</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar;
