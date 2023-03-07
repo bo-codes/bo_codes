381d9a172b8f776c434c1458514b16e1c1abb72f
elias <99310710+bo-codes@users.noreply.github.com>
2023-03-07 04:59:21 -0800

    still reformatting for the change to routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   // // Serve the frontend's index.html file at all other routes NOT starting with /api
   app.get(/^(?!\/?api).*/, (req, res) => {
     // res.cookie("XSRF-TOKEN", req.csrfToken());
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../../frontend", "build", "index.html"));
   });
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
