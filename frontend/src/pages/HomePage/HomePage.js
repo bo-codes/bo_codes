4db747906dcc7be56c021393532ced43f43cf022
elias <99310710+bo-codes@users.noreply.github.com>
2023-03-07 01:47:40 -0800

    minor changes

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,15 +1,13 @@
 import "./HomePage.css";
-import GithubSection from "../../components/GithubSection/GithubSection"
 
-
-function HomePage({data, loading}) {
+function HomePage({ data, loading }) {
   return (
     <div className="section-wrapper">
       <div id="github-container" className="section">
-        <GithubSection data={data} loading={loading}/>
+        {/* <GithubSection data={data} loading={loading} /> */}
       </div>
     </div>
-  )
+  );
 }
 
 export default HomePage;
