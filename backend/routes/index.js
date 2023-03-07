89c0d92123f800089efdce9772b02e1fb16f00cd
elias <99310710+bo-codes@users.noreply.github.com>
2023-03-07 04:51:45 -0800

    still reformatting for the change to routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -19,11 +19,11 @@ if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
     // res.cookie("XSRF-TOKEN", req.csrfToken());
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../../frontend", "build", "index.html"));
   });
 
   // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve("../frontend/build")));
+  router.use(express.static(path.resolve("../../frontend/build")));
 
   // // Serve the frontend's index.html file at all other routes NOT starting with /api
   // app.get(/^(?!\/?api).*/, (req, res) => {
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get("*", (req, res) => {
     // res.cookie("XSRF-TOKEN", req.csrfToken());
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../../frontend", "build", "index.html"));
   })
 }
 // ---------- BOILER PLATE TO RUN REACT APP IN PRODUCTION ---------- ^^
