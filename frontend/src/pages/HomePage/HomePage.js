50b3d376b9f038c0c39b4dadce3de07433bc0892
elias <99310710+bo-codes@users.noreply.github.com>
2023-03-07 15:44:14 -0800

    figuring out missing connection between heroku config vars

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -3,6 +3,7 @@ import "./HomePage.css";
 function HomePage({ data, loading }) {
   return (
     <div className="section-wrapper">
+      {console.log(process.env, "PROCESS ENV")}
       <div id="github-container" className="section">
         {/* <GithubSection data={data} loading={loading} /> */}
       </div>
