d74ba39fd4be4c3f20c85ac11632eb0b25b523d4
elias <99310710+bo-codes@users.noreply.github.com>
2023-03-07 02:28:00 -0800

    minor changes

--- backend/index.js
+++ backend/index.js
@@ -24,12 +24,12 @@ if (process.env.NODE_ENV === "production") {
   });
 
   // Serve the static assets in the frontend's build folder
-  app.use(express.static(path.resolve("../frontend/build/static")));
+  app.use(express.static(path.resolve("../frontend/build")));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   app.get(/^(?!\/?api).*/, (req, res) => {
     // res.cookie("XSRF-TOKEN", req.csrfToken());
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
   });
 }
 // ---------- BOILER PLATE TO RUN REACT APP IN PRODUCTION ---------- ^^
