929a85122a8684460e22c71f19a9b1004e75e2f3
elias <99310710+bo-codes@users.noreply.github.com>
2023-03-07 02:15:19 -0800

    added start script to backend package.json

--- backend/index.js
+++ backend/index.js
@@ -11,24 +11,24 @@ app.use(cors());
 
 const token = process.env.GH_AUTH;
 
-const path = require("path");
-console.log(path.resolve(__dirname, "/frontend", "build", "index.html"));
+// const path = require("path");
+// console.log(path.resolve(__dirname, "/frontend", "build", "index.html"));
 // ---------- BOILER PLATE TO RUN REACT APP IN PRODUCTION ---------- vv
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
   // Serve the frontend's index.html file at the root route
   app.get("/", (req, res) => {
     // res.cookie("XSRF-TOKEN", req.csrfToken());
-    res.sendFile(path.resolve(__dirname, "/frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
   });
 
   // Serve the static assets in the frontend's build folder
-  app.use(express.static(path.resolve("/frontend/build")));
+  app.use(express.static(path.resolve("../frontend/build")));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   app.get(/^(?!\/?api).*/, (req, res) => {
     // res.cookie("XSRF-TOKEN", req.csrfToken());
-    res.sendFile(path.resolve(__dirname, "/frontend/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
   });
 }
 // ---------- BOILER PLATE TO RUN REACT APP IN PRODUCTION ---------- ^^
