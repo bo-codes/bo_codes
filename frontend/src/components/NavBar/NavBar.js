c0e67ce17b972a4d095ada63eef3d80414ddb92c
elias <99310710+bo-codes@users.noreply.github.com>
2023-03-10 16:33:19 -0800

    changes to navbar, removed skills and contact page and moved homepage and centered rest of navlinks

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,21 +1,24 @@
 import { NavLink } from "react-router-dom";
+import {AiFillHome} from "react-icons/ai"
+
 import "./NavBar.css";
 
 function NavBar() {
-
   return (
     <div id="navbar-container">
       <div id="list-div">
-        <ul id="navbar-list">
+        <ul id="navbar-home">
           <li>
             <NavLink
               to="/"
               className="navbar-option"
               // style={({ isActive }) => ( isActive && {textShadow: "0px 0px 40px rgba(255, 255, 255, 1), 0 0 .6em white, 0 0 1em white"})}
             >
-              /
+              <AiFillHome />
             </NavLink>
           </li>
+        </ul>
+        <ul id="navbar-list">
           <li>
             <NavLink
               to="/about"
@@ -30,19 +33,30 @@ function NavBar() {
               EXPERIENCE
             </NavLink>
           </li>
-          <li>
+          {/* <li>
             <NavLink to="/skills" className="navbar-option">
               SKILLS
             </NavLink>
-          </li>
+          </li> */}
           <li>
             <NavLink to="/projects" className="navbar-option">
               PROJECTS
             </NavLink>
           </li>
-          <li>
+          {/* <li>
             <NavLink to="/contact" className="navbar-option">
               CONTACT
+            </NavLink>
+          </li> */}
+        </ul>
+        <ul style={{ visibility: "hidden" }}>
+          <li>
+            <NavLink
+              to="/"
+              className="navbar-option"
+              // style={({ isActive }) => ( isActive && {textShadow: "0px 0px 40px rgba(255, 255, 255, 1), 0 0 .6em white, 0 0 1em white"})}
+            >
+              /
             </NavLink>
           </li>
         </ul>
