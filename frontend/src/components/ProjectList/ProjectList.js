9f92257ef68a90cc8a532b3d2c296808fc1c1e17
elias <99310710+bo-codes@users.noreply.github.com>
2023-04-05 01:44:56 -0700

    added B0rdle to projectList

--- frontend/src/components/ProjectList/ProjectList.js
+++ frontend/src/components/ProjectList/ProjectList.js
@@ -2,6 +2,7 @@ import ProjectItem from "../ProjectItem/ProjectItem";
 import eventzeitImg from "../../img/portfolio/1.jpg"
 import bobogramImg from "../../img/portfolio/2.jpg"
 import gestalt3dImg from "../../img/portfolio/3.png"
+import B0rdleImg from "../../img/portfolio/4.png"
 
 const ProjectList = () => {
   const projects = [
@@ -22,6 +23,12 @@ const ProjectList = () => {
       img: gestalt3dImg,
       company: "Gestalt 3D",
       link: "https://gestalt3d.com/",
+    },
+    {
+      name: "B0rdle",
+      img: B0rdleImg,
+      company: "Personal",
+      link: "https://b0rdle.netlify.app/",
     },
     // { name: "Almira Akin", img: "", company: "Almira Akin" },
   ];
