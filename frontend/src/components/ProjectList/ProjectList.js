2f38cb82a48143f9ca5d87355d7791ca1af8f33c
elias <99310710+bo-codes@users.noreply.github.com>
2023-06-11 18:55:58 -0700

    added projects

--- frontend/src/components/ProjectList/ProjectList.js
+++ frontend/src/components/ProjectList/ProjectList.js
@@ -3,6 +3,8 @@ import eventzeitImg from "../../img/portfolio/1.jpg"
 import bobogramImg from "../../img/portfolio/2.jpg"
 import gestalt3dImg from "../../img/portfolio/3.png"
 import B0rdleImg from "../../img/portfolio/4.png"
+import ilaydaImg from "../../img/portfolio/5.png"
+import textScanImg from "../../img/portfolio/6.gif"
 
 const ProjectList = () => {
   const projects = [
@@ -29,6 +31,18 @@ const ProjectList = () => {
       img: B0rdleImg,
       company: "Personal",
       link: "https://b0rdle.netlify.app/",
+    },
+    {
+      name: "Ilayda Magrann",
+      img: ilaydaImg,
+      company: "Freelance",
+      link: "https://ilaydamagrann.netlify.app/",
+    },
+    {
+      name: "text-scan",
+      img: textScanImg,
+      company: "NPM Package",
+      link: "https://www.npmjs.com/package/text-scan",
     },
     // { name: "Almira Akin", img: "", company: "Almira Akin" },
   ];
